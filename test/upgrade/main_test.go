4fde059f8f8bf161051eeea90f483a5d14616e77
Martin Gencur <mgencur@redhat.com>
2023-05-18 12:22:39 +0200

    TestMain for upgrade tests

---
+++ test/upgrade/main_test.go
@@ -0,0 +1,57 @@
+//go:build upgrade
+// +build upgrade
+
+/*
+ * Copyright 2023 The Knative Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package upgrade
+
+import (
+	"flag"
+	"log"
+	"os"
+	"testing"
+
+	// Uncomment the following line to load the gcp plugin (only required to authenticate against GKE clusters).
+	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+
+	"knative.dev/pkg/injection"
+	_ "knative.dev/pkg/system/testing"
+	"knative.dev/pkg/test/zipkin"
+
+	pkgTest "knative.dev/pkg/test"
+	"knative.dev/reconciler-test/pkg/environment"
+)
+
+var global environment.GlobalEnvironment
+
+func TestMain(m *testing.M) {
+	flag.Parse()
+
+	cfg, err := pkgTest.Flags.ClientConfig.GetRESTConfig()
+	if err != nil {
+		log.Fatal("Error building client config: ", err)
+	}
+	ctx, startInformers := injection.EnableInjectionOrDie(nil, cfg) //nolint
+
+	global = environment.NewGlobalEnvironment(ctx, startInformers)
+
+	// Run the tests.
+	os.Exit(func() int {
+		defer zipkin.CleanupZipkinTracingSetup(log.Printf)
+		return m.Run()
+	}())
+}
