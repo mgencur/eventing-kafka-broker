10c923f69a0519ab781b803bdd29a0c0be972c1c
Martin Gencur <mgencur@redhat.com>
2025-01-02 13:55:28 +0100

    Implement brokerEventVersionNotUpgraded using REKT

--- test/e2e_new/broker_conformance_test.go
+++ test/e2e_new/broker_conformance_test.go
@@ -22,6 +22,8 @@ package e2e_new
 import (
 	"testing"
 
+	cloudevents "github.com/cloudevents/sdk-go/v2"
+	cloudeventstest "github.com/cloudevents/sdk-go/v2/test"
 	// Uncomment the following line to load the gcp plugin (only required to authenticate against GKE clusters).
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	"knative.dev/pkg/system"
@@ -29,12 +31,24 @@ import (
 
 	brokerconfigmap "knative.dev/eventing-kafka-broker/test/rekt/resources/configmap/broker"
 	"knative.dev/eventing/pkg/apis/eventing"
+	eventingv1 "knative.dev/eventing/pkg/apis/eventing/v1"
 	"knative.dev/eventing/test/rekt/features/broker"
 	b "knative.dev/eventing/test/rekt/resources/broker"
+	"knative.dev/eventing/test/rekt/resources/trigger"
 	"knative.dev/reconciler-test/pkg/environment"
+	"knative.dev/reconciler-test/pkg/eventshub"
 	"knative.dev/reconciler-test/pkg/feature"
+
+	eventassert "knative.dev/reconciler-test/pkg/eventshub/assert"
 	"knative.dev/reconciler-test/pkg/k8s"
 	"knative.dev/reconciler-test/pkg/knative"
+	"knative.dev/reconciler-test/pkg/resources/service"
+	"knative.dev/reconciler-test/pkg/state"
+)
+
+const (
+	defaultEventType   = "dev.knative.test.event"
+	defaultEventSource = "source"
 )
 
 func TestBrokerConformance(t *testing.T) {
@@ -61,6 +75,7 @@ func TestBrokerConformance(t *testing.T) {
 
 	env.TestSet(ctx, t, broker.ControlPlaneConformance(brokerName, opts...))
 	env.TestSet(ctx, t, broker.DataPlaneConformance(brokerName))
+	env.TestSet(ctx, t, DataPlaneConformance(brokerName))
 }
 
 func BrokerCreateConfigMap(configName string) *feature.Feature {
@@ -71,4 +86,138 @@ func BrokerCreateConfigMap(configName string) *feature.Feature {
 	))
 
 	return f
+}
+
+func DataPlaneConformance(brokerName string) *feature.FeatureSet {
+	fs := &feature.FeatureSet{
+		Name: "Knative Kafka Broker Specification - Data Plane",
+		Features: []*feature.Feature{
+			DataPlaneIngress(brokerName),
+		},
+	}
+
+	return fs
+}
+
+func DataPlaneIngress(brokerName string) *feature.Feature {
+	f := feature.NewFeatureNamed("Ingress")
+
+	//loggerName := "logger-pod"
+	//secondLoggerName := "second-logger-pod"
+	sink1 := feature.MakeRandomK8sName("sink-1")
+	sink2 := feature.MakeRandomK8sName("sink-2")
+	sinkTransformer := feature.MakeRandomK8sName("sink-transformer")
+	trigger1 := feature.MakeRandomK8sName("trigger-1")
+	trigger2 := feature.MakeRandomK8sName("trigger-2")
+	triggerTransformer := feature.MakeRandomK8sName("trigger-transformer")
+	triggerReply := feature.MakeRandomK8sName("trigger-reply")
+	replySource := feature.MakeRandomK8sName("origin-for-reply")
+
+	// Construct cloudevent message after transformation
+	transformedEventType := "reply-check-type"
+	transformedEventSource := "reply-check-source"
+	transformedBody := `{"msg":"Transformed!"}`
+
+	//baseEvent := cloudevents.NewEvent()
+	//baseEvent.SetID(eventID)
+	//baseEvent.SetType(testlib.DefaultEventType)
+	//baseEvent.SetSource(baseSource)
+	//baseEvent.SetSpecVersion("1.0")
+	//body := fmt.Sprintf(`{"msg":%q}`, eventID)
+	//baseEvent.SetData(cloudevents.ApplicationJSON, []byte(body))
+
+	f.Setup("Set names", func(ctx context.Context, t feature.T) {
+		state.SetOrFail(ctx, t, "brokerName", brokerName)
+		state.SetOrFail(ctx, t, "sink1", sink1)
+		state.SetOrFail(ctx, t, "sink2", sink2)
+		state.SetOrFail(ctx, t, "sinkTransformer", sinkTransformer)
+	})
+
+	f.Setup("install sink-1", eventshub.Install(sink1, eventshub.StartReceiver))
+	f.Setup("install sink-2", eventshub.Install(sink2, eventshub.StartReceiver))
+	f.Setup("install sink-transformer", eventshub.Install(sinkTransformer,
+		eventshub.ReplyWithTransformedEvent(transformedEventType, transformedEventSource, transformedBody),
+		eventshub.StartReceiver),
+	)
+
+	filter1 := eventingv1.TriggerFilterAttributes{
+		"type": eventingv1.TriggerAnyFilter,
+	}
+
+	f.Setup("install trigger1", trigger.Install(
+		trigger1,
+		trigger.WithBrokerName(brokerName),
+		trigger.WithFilter(filter1),
+		trigger.WithSubscriber(service.AsKReference(sink1), ""),
+	))
+	f.Setup("trigger1 goes ready", trigger.IsReady(trigger1))
+
+	filter2 := eventingv1.TriggerFilterAttributes{
+		"source": "filtered-event", // Reuse name of "source"?
+	}
+
+	f.Setup("install trigger2", trigger.Install(
+		trigger2,
+		trigger.WithBrokerName(brokerName),
+		trigger.WithFilter(filter2),
+		trigger.WithSubscriber(service.AsKReference(sink2), ""),
+	))
+	f.Setup("trigger2 goes ready", trigger.IsReady(trigger2))
+
+	filterTransformer := eventingv1.TriggerFilterAttributes{
+		"source": replySource,
+		"type":   defaultEventType,
+	}
+
+	f.Setup("install trigger transformer", trigger.Install(
+		triggerTransformer,
+		trigger.WithBrokerName(brokerName),
+		trigger.WithFilter(filterTransformer),
+		trigger.WithSubscriber(service.AsKReference(sinkTransformer), ""),
+	))
+	f.Setup("trigger transformer goes ready", trigger.IsReady(triggerTransformer))
+
+	filterReply := eventingv1.TriggerFilterAttributes{
+		"source": transformedEventSource,
+		"type":   transformedEventType,
+	}
+
+	f.Setup("install trigger reply", trigger.Install(
+		triggerReply,
+		trigger.WithBrokerName(brokerName),
+		trigger.WithFilter(filterReply),
+		trigger.WithSubscriber(service.AsKReference(sink1), ""),
+	))
+	f.Setup("trigger transformer goes ready", trigger.IsReady(triggerTransformer))
+
+	f.Stable("Conformance").
+		ShouldNot("The Broker SHOULD NOT perform an upgrade of the produced event's CloudEvents version.",
+			brokerEventVersionNotUpgraded)
+	return f
+}
+
+func brokerEventVersionNotUpgraded(ctx context.Context, t feature.T) {
+	brokerName := state.GetStringOrFail(ctx, t, "brokerName")
+	sink1 := state.GetStringOrFail(ctx, t, "sink1")
+
+	event := cloudevents.NewEvent()
+	event.SetID("no-upgrade")
+	event.SetType(defaultEventType)
+	event.SetSource(defaultEventSource)
+	event.SetSpecVersion("1.0")
+	body := fmt.Sprintf(`{"msg":%q}`, eventID)
+	event.SetData(cloudevents.ApplicationJSON, []byte(body))
+	event.Context = event.Context.AsV03()
+
+	eventshub.Install(
+		source,
+		eventshub.StartSenderToResource(broker.GVR(), brokerName),
+		eventshub.InputEvent(event),
+	)(ctx, t)
+
+	eventMatcher := eventassert.MatchEvent(
+		cloudeventstest.HasId("no-upgrade"),
+		cloudeventstest.HasSpecVersion("0.3"),
+	)
+	_ = eventshub.StoreFromContext(ctx, sink1).AssertExact(ctx, t, 1, eventMatcher)
 }
